275f661eccef325c81a5698a5e83bab9b466d90a
Hojin Baek <baeghojin4@gmail.com>
2025-01-20 20:31:34 +0900

    스프링부트연습

---
+++ be-practice/src/main/java/com/mee/be/practice/BePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mee.be.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BePracticeApplication.class, args);
+	}
+
+}
